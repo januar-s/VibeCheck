7545d2511f9b777b4e272d8fc4f34ed808d20890
Iliketheflowers <stefanie.hasler@stud.fhgr.ch>
2023-12-15 18:25:38 +0100

    Header Menu button codiert
    Header das image burger raus genommen und die linien codiert so dass sie sich überkreuzen wenn es sich öffnet
    
    burger inhalt und padding gemacht
    
    js hinzugefügt für einen coolen effekt :)

---
+++ Javascript/script.js
@@ -0,0 +1,37 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelectorAll("#menu li").forEach(item => {
+  let interval = null;
+  let originalText = item.textContent;
+
+  item.addEventListener("mouseover", () => {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      item.textContent = originalText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return originalText[index];
+          }
+
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= originalText.length) {
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 30);
+  });
+
+  item.addEventListener("mouseout", () => {
+    clearInterval(interval);
+    item.textContent = originalText;
+  });
+});
+
